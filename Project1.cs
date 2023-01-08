c2b50e7269fcdb67a512be9291ee24dce7e9c325
Kirill <kirill>
2023-01-08 16:06:13 +0500

    написали программу для определение большего числа

--- Project1.cs
+++ Project1.cs
@@ -6,7 +6,7 @@
 
 Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второче число:");
+Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2)
 {
